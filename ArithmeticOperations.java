e4b3043ffbbe79bc4f398147e42efd10add1f8dc
BCAvarshini <varshinijakkali@gmail.com>
2025-04-29 09:51:24 +0530

    description

---
+++ ArithmeticOperations.java
@@ -0,0 +1,13 @@
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        double num1 = 10;
+        double num2 = 5;
+
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+    }
+}
